3202fb7a2725df8d79dfc2303bafb0392309aa41
John Stinson <john.towery.stinson@gmail.com>
2015-11-10 19:46:21 -0600

    fixed arc path math

--- canvas-round-rect.js
+++ canvas-round-rect.js
@@ -1,17 +1,17 @@
 function roundRect(ctx, x,y,width,height,radius, fill) {
   ctx.beginPath();
   ctx.arc(x + radius, y + radius, radius, Math.PI, Math.PI+Math.PI/2);
-  ctx.lineTo(x + radius + (width - (2 * radius)), y);
-  ctx.arc(x + radius + (width - (2 * radius)), y + radius, radius, Math.PI + Math.PI/2, 0);
-  ctx.lineTo(x + width, y + radius + (height - (2 * radius)));
-  ctx.arc(x + radius + (width - (2 * radius)), y + radius + (height - (2 * radius)), radius,0,Math.PI/2)
+  ctx.lineTo(x + width - radius, y);
+  ctx.arc(x + width - radius, y + radius, radius, Math.PI + Math.PI/2, 0);
+  ctx.lineTo(x + width, y + height - radius);
+  ctx.arc(x + width - radius, y + height - radius, radius, 0, Math.PI/2)
   ctx.lineTo(x + radius, y + height)
-  ctx.arc(x + radius, y + radius + (height - (2 * radius)), radius, Math.PI/2, Math.PI)
+  ctx.arc(x + radius, y + height - radius, radius, Math.PI/2, Math.PI)
   ctx.lineTo(x , y + radius)
 
   if(fill) {
     ctx.fill()
   }
-  
+
   ctx.stroke();
 }
